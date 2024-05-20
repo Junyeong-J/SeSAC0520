0fe879cf5f53e14640c27a48b7b8d2c57e2d7024
JunYeong-J <eeot2827@gmail.com>
2024-05-20 09:43:25 +0900

    Initial Commit

---
+++ SeSAC0520/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC0520
+//
+//  Created by 전준영 on 5/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
